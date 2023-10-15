9a072521681bd7c9ec00feeaafb03b7dd2f4b694
レイドー <33325105+elliottophellia@users.noreply.github.com>
2023-10-15 15:02:04 +0700

    Update

--- routers/routes.py
+++ routers/routes.py
@@ -8,17 +8,14 @@ router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def read_items():
-    # Use 'with' statement to automatically close the file after reading
     with open('views/index.html', 'r') as file:
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
 @router.api_route("/api/reverseip", methods=["GET", "POST", "PUT"])
 async def reverseapi(request: Request, domain: str = Form(None)):
-    # Use 'time.perf_counter()' for more accurate timing
     start_time = time.perf_counter()
     
-    # Consolidate the domain fetching logic
     domain = domain or request.query_params.get("domain")
     
     if not domain:
@@ -26,14 +23,12 @@ async def reverseapi(request: Request, domain: str = Form(None)):
     
     response = await fetch_data(domain)
     
-    # Use 'time.perf_counter()' for more accurate timing
     end_time = time.perf_counter()
     req_time = end_time - start_time
     
     if not response:
         return {"ReqStatus": 500}
     
-    # Consolidate the return statement
     return {
         "ReqTime": req_time,
         "ReqStatus": 200,
